d077f3642b1c0cb3f8ad53dbbd5260bfa8f837c7
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2024-03-17 21:13:31 +0300

    fix: handlers are added into the main program

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import os
 
 from dotenv import load_dotenv
-from pyrogram import Client
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+from handlers import start_command, enter_name, enter_username, get_user_state
 
 load_dotenv()
 
-app = Client(
+app: Client = Client(
     'my_account',
     api_id=os.getenv('API_ID'),
     api_hash=os.getenv('API_HASH'),
     bot_token=os.getenv('BOT_TOKEN'),
 )
 
+app.add_handler(
+    MessageHandler(
+        start_command,
+        filters.command('start')
+    )
+)
 
+app.add_handler(
+    MessageHandler(
+        enter_name,
+        filters.text & filters.create(lambda _, __, query: get_user_state(query.from_user.id) == 'enter_name')
+    )
+)
+
+
+app.add_handler(
+    MessageHandler(
+        enter_username,
+        filters.text & filters.create(lambda _, __, query: get_user_state(query.from_user.id) == 'enter_username')
+    )
+)
 app.run()
