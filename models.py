1a711873cb5cb8a118539e6c45baab12af642bc1
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2024-03-17 21:11:58 +0300

    feature: UserStates model is created

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, UUID, String, Text, ForeignKey
+from sqlalchemy import Column, UUID, String, Text, ForeignKey, Integer
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,9 +9,9 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
-    name = Column(String(100), nullable=False, unique=False)
-    username = Column(String(50), nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(100), nullable=True, unique=False)
+    username = Column(String(50), nullable=True, unique=True)
 
 
 class Tasks(Base):
@@ -22,3 +22,10 @@ class Tasks(Base):
     description = Column(Text, nullable=True, unique=False)
     state = Column(String(10), nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+
+
+class UsersStates(Base):
+    __tablename__ = 'usersstates'
+
+    user_id = Column(Integer, primary_key=True, unique=True)
+    state = Column(String(50), nullable=False, unique=False)
