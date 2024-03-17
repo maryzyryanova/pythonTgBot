489267218ac2f739b73755659bac1b15f712b2f8
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2024-03-17 23:18:16 +0300

    feature: new handlers are added

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
-from handlers import start_command, enter_name, enter_username, get_user_state
+from containers import Container
+from handlers import (
+    start_command,
+    enter_name,
+    enter_username,
+    create_task,
+    handle_task_title,
+    handle_task_description
+)
+from services.user_states_service import UserStatesService
 
 load_dotenv()
 
@@ -14,6 +23,10 @@ app: Client = Client(
     bot_token=os.getenv('BOT_TOKEN'),
 )
 
+container = Container()
+container.init_resources()
+container.wire(modules=[__name__])
+
 app.add_handler(
     MessageHandler(
         start_command,
@@ -23,8 +36,16 @@ app.add_handler(
 
 app.add_handler(
     MessageHandler(
+        create_task,
+        filters.command('create_task')
+    )
+)
+
+app.add_handler(
+    MessageHandler(
         enter_name,
-        filters.text & filters.create(lambda _, __, query: get_user_state(query.from_user.id) == 'enter_name')
+        filters.text & filters.create(
+            lambda _, __, query: UserStatesService.get_user_state(query.from_user.id) == 'enter_name')
     )
 )
 
@@ -32,7 +53,28 @@ app.add_handler(
 app.add_handler(
     MessageHandler(
         enter_username,
-        filters.text & filters.create(lambda _, __, query: get_user_state(query.from_user.id) == 'enter_username')
+        filters.text & filters.create(
+            lambda _, __, query: UserStatesService.get_user_state(query.from_user.id) == 'enter_username')
     )
 )
+
+app.add_handler(
+    MessageHandler(
+        handle_task_title,
+        filters.text & filters.create(
+            lambda _, __, query: UserStatesService.get_user_state(query.from_user.id) == 'create_task_title'
+        )
+    )
+)
+
+
+app.add_handler(
+    MessageHandler(
+        handle_task_description,
+        filters.text & filters.create(
+            lambda _, __, query: UserStatesService.get_user_state(query.from_user.id) == 'create_task_description'
+        )
+    )
+)
+
 app.run()
