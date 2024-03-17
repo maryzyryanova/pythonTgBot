6fa35eee4d6c18728f0507ddf1da7205b2544a1c
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2024-03-17 23:16:44 +0300

    feature: a FSM for task creation is implemented

--- fsm.py
+++ fsm.py
@@ -17,3 +17,19 @@ class RegisterFSM(object):
             initial=initial
         )
 
+
+class TaskCreationFSM(object):
+    __states: list[str] = ['new_task', 'create_task_title', 'create_task_description', 'registered']
+    __transitions: list[dict[str, str]] = [
+        {'trigger': 'create_new_task', 'source': 'new_task', 'dest': 'create_task_title'},
+        {'trigger': 'provide_task_title', 'source': 'create_task_title', 'dest': 'create_task_description'},
+        {'trigger': 'provide_task_description', 'source': 'create_task_description', 'dest': 'registered'},
+    ]
+
+    def __init__(self, initial: str = 'new_task'):
+        self.register_machine = Machine(
+            model=self,
+            states=self.__states,
+            transitions=self.__transitions,
+            initial=initial
+        )
